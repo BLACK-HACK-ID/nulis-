5c7dbbed3ad9118b26b33afc6ca711bde9830f2d
BLACK-HACK-ID <81123771+BLACK-HACK-ID@users.noreply.github.com>
2021-03-24 21:34:01 +0800

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,9 @@
+pkg install jq
+pkg upgrade
+pkg update
+pkg install curl
+pkg install python python2 git
+pkg install toilet figlet cowsay
+pip2 install requests
+pip2 install mechanize
+python3 nulisv1.py
